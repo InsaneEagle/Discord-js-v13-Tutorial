412f7ef2efbe3cefdbb52fe24cc3417484807ce1
Insane Eagle <96535338+InsaneEagle@users.noreply.github.com>
2022-05-06 12:40:34 +0530

    Update index.js

--- Step-1 Create Bot/index.js
+++ Step-1 Create Bot/index.js
@@ -9,4 +9,7 @@ config = process.env;
 client.on('ready', () => {
   console.log('Ready!')
 });
+  console.log('Logged in!')
+  client.user.setActivity("Nxtlve Development", {type: "WATCHING"})
+})
 client.login(config.token);
